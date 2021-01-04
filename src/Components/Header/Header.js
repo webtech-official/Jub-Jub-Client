81b0cdf040a36b15fd7ff5e4f19f9f5b9f81b67f
towel1017 <towel1017@naver.com>
2021-01-04 18:28:25 +0900

    [ADD] Header Modal Open Logic

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,14 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
+import { Login, Register } from "..";
 import { logo } from "../../img/imgExport";
-import { Button } from "../../Styles";
+import { Button, Modal } from "../../Styles";
 import { ButtonWrapper, HeaderWrapper, Logo } from "./Styled";
 
 const Header = () => {
+  const [is_open, setOpen] = useState({
+    open: false,
+    component: null,
+  });
   const token = window.localStorage.getItem("token");
   const HeaderMenu = useMemo(() => {
     return token ? (
@@ -13,8 +18,22 @@ const Header = () => {
       </>
     ) : (
       <>
-        <Button backGroundColor="black">로그인</Button>
-        <Button backGroundColor="black">회원가입</Button>
+        <Button
+          backGroundColor="black"
+          onClick={() => {
+            setOpen({ open: true, component: "login" });
+          }}
+        >
+          로그인
+        </Button>
+        <Button
+          backGroundColor="black"
+          onClick={() => {
+            setOpen({ open: true, component: "register" });
+          }}
+        >
+          회원가입
+        </Button>
       </>
     );
   }, [token]);
@@ -22,6 +41,15 @@ const Header = () => {
     <HeaderWrapper>
       <Logo src={logo} />
       <ButtonWrapper>{HeaderMenu}</ButtonWrapper>
+      <Modal
+        is_open={is_open.open}
+        setOpen={() => {
+          setOpen({ open: false });
+        }}
+      >
+        {is_open.component === "login" && <Login />}
+        {is_open.component === "register" && <Register />}
+      </Modal>
     </HeaderWrapper>
   );
 };
