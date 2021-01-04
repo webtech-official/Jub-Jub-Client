53155ec6db2022c0691171a5b074efe36180c0cb
towel1017 <towel1017@naver.com>
2021-01-04 22:19:08 +0900

    [Add] Item Page

--- src/Components/Lookup/Lookup.js
+++ src/Components/Lookup/Lookup.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "./dummy.json";
+import ItemPage from "./ItemPage/ItemPage";
 import LookupItem from "./LookupItem/LookupItem";
 
 const Lookup = () => {
-  const LookupItemList = data.map((item) => <LookupItem itemInfo={item} />);
-  return <>{LookupItemList}</>;
+  const [page, setPage] = useState(1);
+  const LookupItemList = data
+    .filter((item) => item.id <= page * 5 && item.id > (page - 1) * 5)
+    .map((item) => <LookupItem itemInfo={item} />);
+  return (
+    <>
+      <div>번호 | 카테고리 | 품명 | 수량</div>
+      {LookupItemList}
+      <ItemPage id={page} setId={setPage} length={data.length} />
+    </>
+  );
 };
 
 export default Lookup;
