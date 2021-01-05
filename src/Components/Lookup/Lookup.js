9c23a9607540c0a9074324b5db2d79bd3b0ecdf3
유영재 <a01094741871@gmail.com>
2021-01-05 11:42:37 +0900

    [Update] Lookup Container

--- src/Components/Lookup/Lookup.js
+++ src/Components/Lookup/Lookup.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { data } from "./dummy.json";
 import ItemPage from "./ItemPage/ItemPage";
 import LookupItem from "./LookupItem/LookupItem";
+import { LookupWrapper } from "./Styled";
 
 const Lookup = () => {
   const [page, setPage] = useState(1);
@@ -9,11 +10,11 @@ const Lookup = () => {
     .filter((item) => item.id <= page * 5 && item.id > (page - 1) * 5)
     .map((item) => <LookupItem itemInfo={item} />);
   return (
-    <>
+    <LookupWrapper>
       <div>번호 | 카테고리 | 품명 | 수량</div>
       {LookupItemList}
       <ItemPage id={page} setId={setPage} length={data.length} />
-    </>
+    </LookupWrapper>
   );
 };
 
---
+++ src/Components/Lookup/Styled.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const LookupWrapper = styled.div`
+  width: 1280px;
+  height: 900px;
+  background-color: #ffffff;
+`;
+
+export { LookupWrapper };
