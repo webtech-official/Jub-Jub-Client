59a27c6eebf1e94ee0a169f2436c17218eeb7851
towel1017 <towel1017@naver.com>
2021-01-07 14:22:47 +0900

    [Update] ItemPage Change Templete

--- src/Components/Lookup/Styled.js
+++ src/Components/Lookup/Styled.js
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import { left_arrow, right_arrow } from "../../img/index";
 
 const LookupWrapper = styled.div`
   position: absolute;
@@ -52,38 +51,6 @@ const LookupContainer = styled.div`
   .itemAmount {
     width: 296px;
   }
-  .controlArea {
-    width: 30%;
-    height: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    #left {
-      background-image: url(${left_arrow});
-    }
-    #right {
-      background-image: url(${right_arrow});
-    }
-    .selectedPage {
-      font-weight: bold;
-      text-shadow: 0px 0px 5px #ffffff;
-    }
-    .pageMoveButton {
-      width: 24px;
-      height: 24px;
-      background-size: cover;
-      cursor: pointer;
-    }
-    .pageNumber {
-      width: 60%;
-      display: flex;
-      justify-content: space-between;
-      cursor: pointer;
-      span {
-        font-size: 24px;
-      }
-    }
-  }
 `;
 
 const TitleBox = styled.div`
@@ -101,31 +68,4 @@ const TitleBox = styled.div`
   }
 `;
 
-const ContentBox = styled.div`
-  width: 90%;
-  height: 70%;
-  color: white;
-  background-color: #000000;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .items{
-    height:100%;
-    display: flex;
-    flex-direction: column;
-  }  
-  .itemBox {
-    width: fit-content;
-    height: 20%;
-    display: flex;
-    align-items: center;
-    border-bottom: 2px solid #ffffff;
-    span {
-      font-size: 24px;
-      font-weight: bold;
-    }
-  }
-`;
-
-export { LookupWrapper, LookupContainer, TitleBox, ContentBox };
+export { LookupWrapper, LookupContainer, TitleBox };
