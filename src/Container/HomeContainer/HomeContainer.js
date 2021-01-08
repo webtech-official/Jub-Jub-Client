fb49e1491e5075e2b09fea750e76bbbc46464a4e
유영재 <a01094741871@gmail.com>
2021-01-08 10:38:53 +0900

    [UPDATE] Change Background Components to Design Systems

--- src/Container/HomeContainer/HomeContainer.js
+++ src/Container/HomeContainer/HomeContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Lookup } from "../../Components";
+import Background from "../../Styles/BackGround/Background";
 
 const HomeContainer = () => {
   return (
     <>
       <Lookup />
+      <Background size={1950}/>
     </>
   );
 };
