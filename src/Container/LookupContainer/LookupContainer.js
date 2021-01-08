c8add58cd8045cd6161438b27d464c7f81a03ed3
towel1017 <towel1017@naver.com>
2021-01-08 16:09:22 +0900

    [Create] LookUp Container

---
+++ src/Container/LookupContainer/LookupContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Lookup } from "../../Components/index";
+
+const LookupContainer = () => {
+  return (
+    <>
+      <Lookup />
+    </>
+  );
+};
+
+export default LookupContainer;
