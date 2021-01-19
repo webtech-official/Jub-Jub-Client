fbe5124ed768762c06855e2abf3f499d7085d7ed
iseolin76 <iseolin76@gmail.com>
2021-01-19 13:15:09 +0900

    Add left_arrow_balck, right_arrow_black

--- src/img/index.js
+++ src/img/index.js
@@ -1,7 +1,9 @@
-import logo from "./white_logo.png"
+import logo from "./white_logo.png";
 import snow_globe_dom from "./snow_globe_dom.png";
 import ground from "./globe_ground.png";
 import search_icon from "./search_icon.png";
+import left_arrow_black from "./left_arrow_black.png";
+import right_arrow_black from "./right_arrow_black.png";
 import left_arrow from "./left_arrow.png";
 import right_arrow from "./right_arrow.png";
 import instagram from "./instagram.png";
@@ -17,9 +19,11 @@ export {
   search_icon,
   left_arrow,
   right_arrow,
+  left_arrow_black,
+  right_arrow_black,
   instagram,
   facebook,
   twitter,
   X,
-  TestImg
+  TestImg,
 };
