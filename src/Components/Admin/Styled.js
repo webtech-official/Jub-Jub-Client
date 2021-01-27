d4df2e733a879ef680222ea1c1c3b3fc7aa4ea3d
유영재 <a01094741871@gmail.com>
2021-01-27 11:22:12 +0900

    [Update] Modal

--- src/Components/Admin/Styled.js
+++ src/Components/Admin/Styled.js
@@ -92,7 +92,125 @@ export const AdminMainContainer = styled.div`
 `;
 
 export const ModifyWrapper = styled.div`
-  width: 100px;
+  width: 800px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 10px;
+`;
+
+export const ModifyContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const XContainer = styled.div`
+  padding: 20px 30px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const XMark = styled.img`
+  cursor: pointer;
+`;
+
+export const ModifyContent = styled.div`
+  margin: auto 50px;
+  display: flex;
+  justify-content: space-between;
+  .nameBox {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+`;
+
+export const ModifyImg = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: red;
+`;
+
+export const InputBox = styled.div`
+  width: 350px;
+  height: 200px;
+  border-left: 1px solid #000000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  .inputBox {
+    padding: 3px 0;
+    border-bottom: 1px solid #000000;
+    input {
+      width: 300px;
+      height: 20px;
+      font-size: 20px;
+      border: 0;
+      outline: 0;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 450px;
+`;
+
+export const imgUpload = styled.div`
+  width: 100%;
   height: 100px;
-  background-color: wheat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  input[type="file"] {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 0;
+  }
+  label,
+  input[type="submit"] {
+    width: 135px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border: 1px solid #000000;
+    border-radius: 10px;
+  }
+`;
+
+export const btnBox = styled.div`
+  width: 35%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  span {
+    width: 100px;
+    height: 53px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .modifyBtn {
+    background-color: #000000;
+    color: #ffffff;
+  }
+  .deleteBtn {
+    background-color: #ffffff;
+    color: #000000;
+    border: 1px solid #000000
+  }
 `;
