3501c7e92c7e5fd9a06bee0514d6ed728dfa30c1
towel1017 <towel1017@naver.com>
2021-01-28 20:54:53 +0900

    [Add] Login/Register Class Logic

--- src/assets/Api/Auth.js
+++ src/assets/Api/Auth.js
@@ -1,11 +1,44 @@
+import { methodType, requestApiWithBodyWithoutToken } from "../lib/requestApi";
+import { BASE_URL } from "../../config/config.json";
+import { AUTH } from "../lib/requestUrl";
 class Auth {
-  async login() {
+  async login(email, password) {
     try {
-    } catch (error) {}
+      const body = {
+        email,
+        password,
+      };
+      const response = requestApiWithBodyWithoutToken(
+        BASE_URL,
+        methodType.POST,
+        AUTH.login(),
+        body,
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-  async signup() {
+  async signup(classNumber, email, name, password) {
     try {
-    } catch (error) {}
+      const body = {
+        classNumber,
+        email,
+        name,
+        password,
+      };
+      const response = requestApiWithBodyWithoutToken(
+        BASE_URL,
+        methodType.POST,
+        AUTH.signup(),
+        body,
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
