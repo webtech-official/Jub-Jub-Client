3cdc19e014705c5ac9ee80d4ca01b5cf96b46e25
towel1017 <towel1017@naver.com>
2021-01-29 08:59:41 +0900

    [Add] Backend peristalsis Logic

--- src/assets/Api/Admin.js
+++ src/assets/Api/Admin.js
@@ -1,48 +1,146 @@
+import {
+  methodType,
+  requestApiWithBodyWithToken,
+  requestApiWithoutBodyWithToken,
+} from "../lib/requestApi";
+import { BASE_URL } from "../../config/config.json";
+import { ADMIN } from "../lib/requestUrl";
 class Admin {
   //신청 전체 조회
   async applyView() {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.GET,
+        ADMIN.applyView(),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //신청 승인
-  async approved() {
+  async approved(eqa_Idx) {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.PUT,
+        ADMIN.approved(eqa_Idx),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //기자재 등록
-  async equipmentEnroll() {
+  async equipmentEnroll(content, count, img_equipment, name) {
     try {
-    } catch (error) {}
+      const body = {
+        content,
+        count,
+        img_equipment,
+        name,
+      };
+      const response = requestApiWithBodyWithToken(
+        BASE_URL,
+        methodType.POST,
+        body,
+        ADMIN.equipment(),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //기자재 전체 조회
   async equipmentInfoAll() {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.GET,
+        ADMIN.equipment(),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //기자재 상세 조회
-  async equipmentDetail() {
+  async equipmentDetail(name) {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.GET,
+        ADMIN.equipmentDetail(name),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //기자재 수량 변경
-  async equipmentChangeAmount() {
+  async equipmentChangeAmount(count, name) {
     try {
-    } catch (error) {}
+      const body = {
+        count,
+        name,
+      };
+      const response = requestApiWithBodyWithToken(
+        BASE_URL,
+        methodType.PUT,
+        body,
+        ADMIN.equipmentDetail(name),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //기자재 전체 수정
-  async equipmentAllUpdate() {
+  async equipmentAllUpdate(content, count, img_equipment, newName, oldName) {
     try {
+      const body = {
+        content,
+        count,
+        img_equipment,
+        newName,
+      };
+      const response = requestApiWithBodyWithToken(BASE_URL, methodType.PUT);
     } catch (error) {}
   }
   //신청 거절
-  async reject() {
+  async reject(eqa_Idx) {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.PUT,
+        ADMIN.reject(eqa_Idx),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   //반납
-  async return() {
+  async return(eqa_idx) {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.PUT,
+        ADMIN.return(eqa_idx),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
--- src/assets/Api/Laptop.js
+++ src/assets/Api/Laptop.js
@@ -1,19 +1,83 @@
+import { BASE_URL } from "../../config/config.json";
+import {
+  methodType,
+  requestApiWithBodyWithToken,
+  requestApiWithoutBodyWithToken,
+} from "../lib/requestApi";
+import { LAPTOP } from "../lib/requestUrl";
 class Laptop {
   async laptopInfoAll() {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.GET,
+        LAPTOP.laptop(),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-  async laptopEnroll() {
+  async laptopEnroll(
+    classNumber,
+    laptopBrand,
+    laptopName,
+    laptopSerialNumber,
+    specIdx,
+    studentName
+  ) {
     try {
-    } catch (error) {}
+      const body = {
+        classNumber,
+        laptopBrand,
+        laptopName,
+        laptopSerialNumber,
+        specIdx,
+        studentName,
+      };
+      const response = requestApiWithBodyWithToken(
+        BASE_URL,
+        methodType.POST,
+        body,
+        LAPTOP.laptop(),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-  async laptopDetail() {
+  async laptopDetail(laptopSerialNumber) {
     try {
-    } catch (error) {}
+      const response = requestApiWithoutBodyWithToken(
+        BASE_URL,
+        methodType.GET,
+        LAPTOP.laptopDetail(laptopSerialNumber),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-  async laptopInfoUpdate() {
+  async laptopInfoUpdate(laptopSerialNumber, laptopBrand, laptopName) {
     try {
-    } catch (error) {}
+      const body = {
+        laptopBrand,
+        laptopName,
+      };
+      const response = requestApiWithBodyWithToken(
+        BASE_URL,
+        methodType.PUT,
+        body,
+        LAPTOP.laptopDetail(laptopSerialNumber),
+        {}
+      );
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
