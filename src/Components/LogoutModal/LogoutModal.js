3ad91469d61834a7fce89c84931c5d48b7ab24a8
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-05-19 12:12:52 +0900

    [UPDATE] Logout 컴포넌트 폴더 분리, 가독성 떨어지는 이름 변경

---
+++ src/Components/LogoutModal/LogoutModal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import Auth from '../../assets/Api/Auth'
+import { X } from '../../img'
+import * as S from './Styled'
+
+const LogoutM = ({toggleModal}) => {
+    const history = useHistory();
+    const handleLogout = () => {
+        history.push("/")
+        Auth.logout().then(res => {
+            console.log(res)
+            if(res.data.code >= 0) {
+                alert(res.data.msg);
+                window.localStorage.clear();
+                toggleModal(false)
+                history.push("/")
+            }
+        })
+    }
+    return (
+        <S.ModalBack>
+            <S.ModalCloseBtnImageWrapper>
+                <S.ModalCloseBtnImage src={X} alt="close_btn" onClick={toggleModal}/>
+            </S.ModalCloseBtnImageWrapper>
+            <S.ModalContentWrapper>
+                <S.ModalText>
+                    로그아웃 하시겠습니까?
+                </S.ModalText>
+                <S.CheckBtn onClick={handleLogout}>
+                    확인
+                </S.CheckBtn>
+            </S.ModalContentWrapper>
+        </S.ModalBack>
+    )
+}
+
+export default LogoutM
