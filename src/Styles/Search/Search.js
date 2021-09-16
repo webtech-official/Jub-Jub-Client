6e26e2582a638d26dcb7e0a6c8ee1542620fe336
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-16 17:19:09 +0900

    Update Search.js
    - description을 category로 수정
    - 카테고리 검색 전부 띄우기

--- src/Styles/Search/Search.js
+++ src/Styles/Search/Search.js
@@ -14,10 +14,10 @@ const Search = ({ search, onChange, size, standard, setStandard }) => {
             onChange(e.target.value);
           }}
         />
-        {standard === 'description' ? (
-          <button className="standard" onClick={() => setStandard('name')}>카테고리 검색</button>
+        {standard === 'category' ? (
+          <button className="standard" onClick={() => setStandard('name')}>카테고리 검색dklsdjfslkfjlsk</button> // 아무 의미없는 문자열 같아도 사실 의미가 있습니다.
         ) : (
-          <button className="standard" onClick={() => setStandard('description')}>품명 검색</button>
+          <button className="standard" onClick={() => setStandard('category')}>품명 검색</button>
         )}
         <img src={search_icon} alt="search_icon"></img>
       </div>
