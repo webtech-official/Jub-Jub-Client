d8fd49241c53fd58807cb0b1e75a4fbdb5e3c238
hyeongrok7874 <hyeongrok7874@naver.com>
2021-09-16 15:10:19 +0900

    [Refactoring] Style

--- src/Styles/Search/Styled.js
+++ src/Styles/Search/Styled.js
@@ -10,12 +10,13 @@ export const SearchBox = styled.div`
     display: flex;
     border-bottom: 3px solid #000000;
     .inputBox {
-      padding-top: 10px;
+      padding: 16px;
+      padding-left: 0px;
       width: 95%;
       height: 100%;
       border: 0;
       outline: 0ch;
-      font-size: 24px;
+      font-size: 20px;
       background-color: rgba(0, 0, 0, 0);
     }
   }
